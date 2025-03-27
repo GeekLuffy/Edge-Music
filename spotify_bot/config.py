b7f10cd60f9a041926ebb6610b5b596b9244eff4
GeekLuffy <71143694+GeekLuffy@users.noreply.github.com>
2025-03-27 23:33:51 +0530

    Update config.py

--- spotify_bot/config.py
+++ spotify_bot/config.py
@@ -4,7 +4,7 @@ class Config(object):
     # Pyrogram client config
     API_ID = os.environ.get("API_ID", "27240462")
     API_HASH = os.environ.get("API_HASH", "e6d011e39e3e84cad1e417bda13c7dda")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7270109001:AAEbVp-jTOvG_C65D1HXQ-i92GkvvKRjJss")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     USER_SESSION = os.environ.get("USER_SESSION", "BQBxIQMAjjIva6RLQ2kS7Ioesl9KoKtiaK8OcSwoPlbukpZMCU-OGvoktgKrkckQAU-HEfDrHoGtSknDxtQeM5KSZpHKM4ei-trWKLk4hfxS1MiEvang991RKMYS9QoDg93CTzvl3w8FpZ3qfdWTWRIp5N8WetCE0QzqPh47B-eyXZqgNXgafnRJwELUXtP7l1ta4g5O0O-t0LloTjdotk0TxY_5L0DL9JpPq95BtZ_lmOpVzxVi3db-TUZqDOeVXHS7YKtkNZllV2ckP4JWkhGEncOuUWbqEiMwywABhWGDstAJwyUUp6iC8a5Ar4GKAUsEgAAJdEk3vOn9YX7zHJyjW58ILgAAAAGpcEk8AA")
     ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "7137675580"))
 
